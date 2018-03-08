c01fad96eee237cd8c07d9f236fc073a0c5ecd6e
gzw <zhiwen2720@163.com>
2018-03-08 11:48:55 +0800

    update app config, improve ajaxErrorHandle, print debug info in debug info dialog, add fancyboxOptions.

--- js/appConfig.js
+++ js/appConfig.js
@@ -12,20 +12,33 @@ var appConfig = {
     {
         var status      = XMLHttpRequest.status;
         var statusText  = XMLHttpRequest.statusText;
+        var responseText = XMLHttpRequest.responseText;
 
         var errorMessage = '';
+
         if (getDebugStatus())
         {
             errorMessage = 'Status code: ' + status + ' ' + statusText;
             showMessageBox(errorMessage, '出错了');
+            if ( $('#debugInfoDialog').length )
+            {
+                var info = '<p>' + errorMessage + '</p>';
+                $('#debugInfoDialog .infoContainer').append(info);
+                var info = '<p>' + responseText + '</p>';
+                $('#debugInfoDialog .infoContainer').append(info);
+            }
         }
         else
         {
-            if (status === 408)     // 408 Request Timeout
+            if (status >= 500 && status < 600)
+            {
+                errorMessage = errorMessageList.InternalServerError;
+            }
+            else if (status === 408)     // 408 Request Timeout
             {
                 errorMessage = errorMessageList.RequestTimeout;
             }
-            if (status === 409)     // 409 Conflict
+            else if (status === 409)     // 409 Conflict
             {
                 errorMessage = errorMessageList.RequestConflict;
             }
@@ -33,10 +46,20 @@ var appConfig = {
             {
                 errorMessage = errorMessageList.defaultText;
             }
+
             showMessageBox(errorMessage, '失败');
         }
 
     }),
+
+    fancyboxOptions : {
+        iframe : {
+            css : {
+                width : '90%',
+                height : '90%'
+            }
+        }
+    },
 
     lightboxOption : {
         alwaysShowNavOnTouchDevices : true,
