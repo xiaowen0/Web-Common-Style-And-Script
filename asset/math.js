53518f7b9431be2b43d098041691db7084ba0ab2
gzw <zhiwen2720@163.com>
2018-04-12 16:38:09 +0800

    add somme library.

---
+++ asset/math.js
@@ -0,0 +1,20 @@
+/**
+ * Created by wen on 2018/4/11.
+ */
+
+// math.js
+
+define(['jquery'], function (){
+
+    var add = function (x,y){
+
+        return x+y;
+
+    };
+
+    return {
+
+        add: add
+    };
+
+});
