11f10afba7b22086cf94f7d6df6b6d5b24534eb2
gzw <zhiwen2720@163.com>
2018-04-18 15:33:38 +0800

    add login page.

---
+++ template/memorial_hall/js/login.js
@@ -0,0 +1,9 @@
+/**
+ * Created by wen on 2018/4/12.
+ * page for edit information
+ */
+
+require([
+    'js/module/loginModule'
+]);
+
---
+++ template/memorial_hall/js/module/loginModule.js
@@ -0,0 +1,36 @@
+/**
+ * Created by wen on 2018/4/17.
+ * module for edit information
+ */
+
+define(['vue', 'jquery'], function (Vue){
+
+    var loginForm = new Vue({
+        el : '#loginForm',
+        data : {
+            loginUrl : 'data/login.json',
+            name : '',
+            password : ''
+        },
+        methods : {
+            submit : function(event){
+
+                event.preventDefault();
+
+                var data = {
+                    name : this.name,
+                    password : this.password
+                };
+
+                $.ajax(this.loginUrl, {
+                    dataType : 'json',
+                    type : 'post',
+                    data : data,
+                    success : function(result){
+                    }
+                });
+            }
+        }
+    });
+
+});
