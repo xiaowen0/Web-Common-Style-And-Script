c4e616aa334cd6461df11bd1034f62f79efd48da
gzw <zhiwen2720@163.com>
2018-04-18 15:43:05 +0800

    update select theme page's scripts.

---
+++ template/memorial_hall/js/module/selectThemeModule.js
@@ -0,0 +1,25 @@
+/**
+ * Created by wen on 2018/4/9.
+ */
+
+define(['vue', 'jquery'], function (Vue)
+{
+    var selectThemeContainer = new Vue({
+        el: '#selectThemeContainer',
+        data: {},
+        methods: {
+            selectTheme: function (name)
+            {
+                if ($('#themeStyleSheet').length)
+                {
+                    $('#themeStyleSheet').remove();
+                }
+
+                var themeStyleSheetUrl = '../../style/theme/' + name + '.css';
+                var themeStyleSheetLink = $('<link rel="stylesheet" id="themeStyleSheet"/>')
+                    .attr('href', themeStyleSheetUrl).appendTo(document.head);
+            }
+        }
+    });
+});
+
---
+++ template/memorial_hall/js/selectTheme.js
@@ -0,0 +1,9 @@
+/**
+ * Created by wen on 2018/4/9.
+ */
+
+require([
+    'js/module/pageHeaderModule',
+    'js/module/selectThemeModule'
+]);
+
