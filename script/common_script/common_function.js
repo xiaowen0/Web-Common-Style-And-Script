af58f2a3cdeff8b017a7ca80e10fde29edef936f
zhiwen <zhiwen2720@163.com>
2019-11-28 16:14:31 +0800

    update getLocation function.

--- script/common_script/common_function.js
+++ script/common_script/common_function.js
@@ -6448,7 +6448,7 @@ function getLocation(options)
     var success = options.success || null;
     var error   = options.error || null;
 
-    if (typeof (wx) != 'undefined') // wechat JSSDK
+    if ( isWechat() && typeof (wx) == 'object') // wechat JSSDK
     {
         wx.getLocation({
             type: type, // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
