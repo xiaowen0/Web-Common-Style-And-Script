656db5bc9bcd80932cc4f3678debd041bc6bcd7b
zhiwen <zhiwen2720@163.com>
2020-06-18 09:33:27 +0800

    stringHelper: update replace method, prevent endless loop replacing. add linkTopicWords method.

--- script/common_script/helper/stringHelper.js
+++ script/common_script/helper/stringHelper.js
@@ -1,8 +1,8 @@
 /**
  * helper to handle some string.
  */
-
-import arrayHelper from './array';
+import arrayHelper from './arrayHelper';
+import consoleHelper from './consoleHelper';
 
 export default {
     /**
@@ -32,6 +32,12 @@ export default {
     replace : function(string, find, replace)
     {
         if (find == replace) {
+            return string;
+        }
+
+        if (replace.indexOf(find) >=0 )
+        {
+            consoleHelper.logError('Replace string fail: replace string include find string.');
             return string;
         }
 
@@ -86,46 +92,17 @@ export default {
 
     getRandomNickName : function () {
         var list = [
-            '风月斟酒',
-            '温柔眼眉',
-            '吃不饱的大可爱~',
-            '脸美逼遭罪@',
-            '承诺i',
-            '站在千里之外',
-            '島是海哭碎菂訫',
-            '叫我小透明',
-            '别同情我',
-            '库存已久',
-            '玖忆惜梦',
-            '愿你余生不孤寂',
-            '二智少女',
-            '半夏微凉、不成殇',
-            '爱耍流氓的兔子',
-            '半城樱花半城雨﹌',
-            '野性随风.',
-            '≠无心少女',
-            '葬于她心',
-            '旁观者',
-            '你听我说',
-            '不惯与生人识',
-            '你并不孤单',
-            '青袂宛约',
-            '让我盲目',
-            '夜夜思量',
-            '拖拉机再垃圾也能拖垃圾',
-            '你会腻我何必',
-            '我可爱炸了.',
-            '年少轻狂。',
-            '走过的路',
-            '占有欲',
-            '眠里微光。',
-            '一灯孤',
-            '几支紫钗挽青丝',
-            '遥歌',
-            '泠泠七弦上',
-            '清風与我',
-            '善悪不定夺',
-            '温致如猫'
+            '风月斟酒', '温柔眼眉', '吃不饱的大可爱~', '脸美逼遭罪@',
+            '承诺i', '站在千里之外', '島是海哭碎菂訫', '叫我小透明',
+            '别同情我', '库存已久', '玖忆惜梦', '愿你余生不孤寂',
+            '二智少女', '半夏微凉、不成殇', '爱耍流氓的兔子',
+            '半城樱花半城雨﹌', '野性随风.', '≠无心少女',
+            '葬于她心', '旁观者', '你听我说', '不惯与生人识',
+            '你并不孤单', '青袂宛约', '让我盲目', '夜夜思量',
+            '拖拉机再垃圾也能拖垃圾', '你会腻我何必', '我可爱炸了.',
+            '年少轻狂。', '走过的路', '占有欲', '眠里微光。',
+            '一灯孤', '几支紫钗挽青丝', '遥歌', '泠泠七弦上',
+            '清風与我', '善悪不定夺', '温致如猫'
         ];
         return arrayHelper.getRandomItem(list);
     },
@@ -144,7 +121,62 @@ export default {
         text = text.replace(reg, replaceStr);
 
         return text;
+    },
+
+    /**
+     * add link for topic words
+     * @param   String  text
+     * @param   Object  options     a tag attributies, the keys must standard A tag's DOM attribute name.
+     * href value can be a function, recieve a topic name param, and return a special url.
+     * @return  String
+     */
+    linkTopicWords : function (text, options)
+    {
+        consoleHelper.logDebug('linkTopicWords:');
+
+        // topic regexp
+        var reg = /#[^#]*#/g;
+
+        var topicList = [];
+        var match;
+        while ((match = reg.exec(text)) !== null) {
+            // console.log(`Found ${match[0]} start=${match.index} end=${reg.lastIndex}.`);
+            // expected output: "Found football start=6 end=14."
+            // expected output: "Found foosball start=16 end=24."
+            topicList.push(`${match[0]}`);
+        }
+        topicList = arrayHelper.unique(topicList);
+        consoleHelper.logDebug('topic list: ' + topicList.join(','));
+
+        // for each topic
+        for (var i=0; i<topicList.length; i++)
+        {
+            var topicName = topicList[i];
+
+            // create a tag, and set some properties
+            var linkEle = document.createElement('a');
+            linkEle.innerHTML = topicName;
+            for (var attrItem in options)
+            {
+                if (attrItem === 'href' && typeof(options[attrItem]) === 'function')
+                {
+                    linkEle[attrItem] = options[attrItem](topicName);
+                }
+                else
+                {
+                    linkEle[attrItem] = options[attrItem];
+                }
+            }
+
+            // replacement string include finding string, then use a middle variable.
+            // example: #hello# -> REPLACEMENT_HELLO -> <a href="">#hello#</a>
+            var key = 'REPLACEMENT_' + topicName.toUpperCase();
+            text = this.replace(text, topicName, key);
+            text = this.replace(text, key, linkEle.outerHTML);
+        }
+
+        consoleHelper.logDebug('text: ' + text);
+        return text;
     }
 
 }
-
