aafaa18d13a2fcd615863516aece1a3242cb1233
zhiwen <zhiwen2720@163.com>
2020-09-11 17:11:03 +0800

    add some utils.

---
+++ util/dataMixedUtils.js
@@ -0,0 +1,19 @@
+import arrayHelper from './arrayHelper';
+import numberHelper from './numberHelper';
+import objectHelper from './objectHelper';
+import stringHelper from './stringHelper';
+import htmlHelper from './htmlHelper';
+import timeHelper from './timeHelper';
+import urlHelper from './urlHelper';
+
+export default {
+
+    array : arrayHelper,
+    number : numberHelper,
+    object : objectHelper,
+    string : stringHelper,
+    html : htmlHelper,
+    time : timeHelper,
+    url : urlHelper
+
+};
---
+++ util/platformApiUtils.js
@@ -0,0 +1,13 @@
+import browserHelper from './browserHelper';
+import consoleHelper from './consoleHelper';
+import deviceHelper from './deviceHelper';
+import domHelper from './domHelper';
+import windowHelper from './windowHelper';
+
+export default {
+    browser : browserHelper,
+    console : consoleHelper,
+    device : deviceHelper,
+    dom : domHelper,
+    window : windowHelper
+};
